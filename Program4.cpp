5616d74e49a7369e681495c23cc951d5bab12582
Dawam <100006614+AzmiDawami24@users.noreply.github.com>
2023-09-12 19:57:07 +0700

    Program kelipatan 2
    Program untuk memunculkan angka kelipatan 2 dari 1 sampai 10

---
+++ Program4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Perulangan kelipatan 2" << endl;
+	cout << "Oleh Azmi Akhmad Dawami" << endl;
+	
+	int a;
+	for(a = 1; a <= 10; a++){
+		if(a%2==0){
+			cout << a << endl;
+		}
+	}
+}
