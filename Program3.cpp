c328eb60f605bb7e71043beb0f2494765fe673f9
Dawam <100006614+AzmiDawami24@users.noreply.github.com>
2023-09-12 19:54:31 +0700

    Perulangan buat munculin angka 10 sampai 5
    Perulangan yang diulang-ulang -_-

---
+++ Program3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Program 3 Perulangan" << endl;
+	cout << "Oleh Azmi Akhmad Dawami" << endl;
+	
+	int a;
+	for(a = 10; a > 5; a--){
+		cout << a << endl;
+	}
+}
