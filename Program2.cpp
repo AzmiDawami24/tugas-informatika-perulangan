f57006c23a42c80080507fcf943ca153aa5aa5fb
Dawam <100006614+AzmiDawami24@users.noreply.github.com>
2023-09-12 19:51:44 +0700

    Munculin angka dari angka 1 sampai 5
    Sudah dijelaskan di judul -_-

---
+++ Program2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Program 2 perulangan" << endl;
+	cout << "Oleh Azmi Akhmad Dawami" << endl;
+	
+	int a;
+	for(a=1; a<=5; a++){
+		cout << a << endl;
+	}
+}
