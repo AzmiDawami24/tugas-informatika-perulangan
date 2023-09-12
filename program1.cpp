5904db483bdefee06cd941b76a73666b518c4662
Dawam <100006614+AzmiDawami24@users.noreply.github.com>
2023-09-12 19:48:00 +0700

    "belajar for"
    cuma nulisin tulisan "belajar for" 5 kali

---
+++ program1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a;
+	cout << "Program 1 Perulangan" << endl;
+	cout << "Oleh Azmi Akhmad Dawami" << endl;
+	for(a = 0; a < 5; a++){
+		cout << "Belajar for" << endl;
+	}
+}
